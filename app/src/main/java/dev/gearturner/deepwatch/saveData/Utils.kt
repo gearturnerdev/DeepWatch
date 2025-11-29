b8de3eaf8d55379008acd4a2f18654e5cbcf79d4
ThisIsHP64 <thisishp64@gmail.com>
2025-11-29 13:52:52 -0500

    implement pss10

--- app/src/main/java/dev/gearturner/deepwatch/saveData/Utils.kt
+++ app/src/main/java/dev/gearturner/deepwatch/saveData/Utils.kt
@@ -4,13 +4,28 @@ import android.content.Context
 import com.google.gson.Gson
 import java.io.File
 
-fun saveSliderValue(context: Context, value: Int) {
+fun saveSliderValue(context: Context, values: List<Int>) {
     val gson = Gson()
-    val data = mapOf("stress_level" to value);
+
+    val toReverse = intArrayOf(3, 4, 6, 7)
+    val correctedValues = values.mapIndexed { index, value ->
+        if(index in toReverse) 4 - value else value
+    }
+
+    val totalScore = correctedValues.sum()
+    val stressLevel = if(totalScore < 14) {
+        "low"
+    } else if(totalScore < 27) {
+        "moderate"
+    } else {
+        "high"
+    }
+    val data = correctedValues.mapIndexed { index, value ->
+        "q${index + 1}" to value
+    }.toMap() + ("score" to totalScore) + ("stressLevel" to stressLevel)
 
     val jsonString = gson.toJson(data)
     val file = File(context.filesDir, "stress_data.json")
 
     file.writeText(jsonString)
-
 }
--- app/src/main/java/dev/gearturner/deepwatch/screens/SliderScreen.kt
+++ app/src/main/java/dev/gearturner/deepwatch/screens/SliderScreen.kt
@@ -10,12 +10,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -31,68 +33,257 @@ import kotlin.math.roundToInt
 import dev.gearturner.deepwatch.saveData.saveSliderValue
 
 @Composable
-@Preview(showBackground = true, showSystemUi = true)
 fun SliderScreen(navController: NavHostController) {
-    var sliderValue by remember { mutableStateOf(3f) }
-
-    Box(
-        modifier = Modifier.fillMaxSize().padding(18.dp)
+    var sliderValues = remember {
+        mutableStateListOf(3f, 3f, 3f, 3f, 3f, 3f, 3f, 3f, 3f, 3f)
+    }
+    LazyColumn(
+        modifier = Modifier
+            .padding(horizontal = 24.dp, vertical = 96.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(96.dp)
     ) {
-        Column(
-            modifier = Modifier.align(Alignment.Center),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+        item {
             Text(
-                text = "How would you rate your stress today?",
+                text = "Answer the following questions based on your experience today.",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                lineHeight = 32.sp
+            )
+        }
+        item {
+            Text(
+                text = "How often have you been upset because of something that happened unexpectedly?",
                 fontSize = 20.sp,
                 textAlign = TextAlign.Center
             )
             Spacer(modifier = Modifier.height(8.dp))
             Slider(
-                value = sliderValue,
-                onValueChange = { sliderValue = it },
+                value = sliderValues[0],
+                onValueChange = { sliderValues[0] = it },
                 valueRange = 1f..5f,
                 steps = 3,
                 onValueChangeFinished = {
-                    sliderValue = sliderValue.roundToInt().toFloat()
+                    sliderValues[0] = sliderValues[0].roundToInt().toFloat()
                 }
             )
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(4.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                for (num in 1..5) {
-                    Text(
-                        text = num.toString(),
-                    )
+            SliderDescriptor()
+        }
+        item {
+            Text(
+                text = "How often have you felt that you were unable to control the important things in your life?",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Slider(
+                value = sliderValues[1],
+                onValueChange = { sliderValues[1] = it },
+                valueRange = 1f..5f,
+                steps = 3,
+                onValueChangeFinished = {
+                    sliderValues[1] = sliderValues[1].roundToInt().toFloat()
                 }
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(4.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
+            )
+            SliderDescriptor()
+        }
+        item {
+            Text(
+                text = "How often have you felt nervous and stressed?",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Slider(
+                value = sliderValues[2],
+                onValueChange = { sliderValues[2] = it },
+                valueRange = 1f..5f,
+                steps = 3,
+                onValueChangeFinished = {
+                    sliderValues[2] = sliderValues[2].roundToInt().toFloat()
+                }
+            )
+            SliderDescriptor()
+        }
+        item {
+            Text(
+                text = "How often have you felt confident about your ability to handle your personal problems?",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Slider(
+                value = sliderValues[3],
+                onValueChange = { sliderValues[3] = it },
+                valueRange = 1f..5f,
+                steps = 3,
+                onValueChangeFinished = {
+                    sliderValues[3] = sliderValues[3].roundToInt().toFloat()
+                }
+            )
+            SliderDescriptor()
+        }
+        item {
+            Text(
+                text = "How often have you felt that things were going your way?",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Slider(
+                value = sliderValues[4],
+                onValueChange = { sliderValues[4] = it },
+                valueRange = 1f..5f,
+                steps = 3,
+                onValueChangeFinished = {
+                    sliderValues[4] = sliderValues[4].roundToInt().toFloat()
+                }
+            )
+            SliderDescriptor()
+        }
+        item {
+            Text(
+                text = "How often have you found that you could not cope with all the things that you had to do?",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Slider(
+                value = sliderValues[5],
+                onValueChange = { sliderValues[5] = it },
+                valueRange = 1f..5f,
+                steps = 3,
+                onValueChangeFinished = {
+                    sliderValues[5] = sliderValues[5].roundToInt().toFloat()
+                }
+            )
+            SliderDescriptor()
+        }
+        item {
+            Text(
+                text = "How often have you been able to control irritations in your life?",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Slider(
+                value = sliderValues[6],
+                onValueChange = { sliderValues[6] = it },
+                valueRange = 1f..5f,
+                steps = 3,
+                onValueChangeFinished = {
+                    sliderValues[6] = sliderValues[6].roundToInt().toFloat()
+                }
+            )
+            SliderDescriptor()
+        }
+        item {
+            Text(
+                text = "How often have you felt that you were on top of things?",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Slider(
+                value = sliderValues[7],
+                onValueChange = { sliderValues[7] = it },
+                valueRange = 1f..5f,
+                steps = 3,
+                onValueChangeFinished = {
+                    sliderValues[7] = sliderValues[7].roundToInt().toFloat()
+                }
+            )
+            SliderDescriptor()
+        }
+        item {
+            Text(
+                text = "How often have you been angered because of things that happened that were outside of your control?",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Slider(
+                value = sliderValues[8],
+                onValueChange = { sliderValues[8] = it },
+                valueRange = 1f..5f,
+                steps = 3,
+                onValueChangeFinished = {
+                    sliderValues[8] = sliderValues[8].roundToInt().toFloat()
+                }
+            )
+            SliderDescriptor()
+        }
+        item {
+            Text(
+                text = "How often have you felt difficulties were piling up so high that you could not overcome them?",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Slider(
+                value = sliderValues[9],
+                onValueChange = { sliderValues[9] = it },
+                valueRange = 1f..5f,
+                steps = 3,
+                onValueChangeFinished = {
+                    sliderValues[9] = sliderValues[9].roundToInt().toFloat()
+                }
+            )
+            SliderDescriptor()
+        }
+        item {
+            Button(
+                onClick = {
+                    saveSliderValue(navController.context, sliderValues.map { it.toInt() })
+                    navController.popBackStack()
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF1976D2)
+                )
             ) {
-                Text("None")
-                Text("Very bad")
+                Text("Submit")
             }
+        }
+    }
 
-        }
-        Button(
-            onClick = {
-                saveSliderValue(navController.context, sliderValue.roundToInt())
-                navController.popBackStack()
-            },
-            modifier = Modifier
-                .align(Alignment.Center)
-                .offset(y = 200.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF1976D2)
+}
+
+@Composable
+fun SliderDescriptor() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        for (num in 0..4) {
+            Text(
+                text = num.toString(),
             )
-        ) {
-            Text("Submit")
         }
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            textAlign = TextAlign.Start,
+            fontSize = 14.sp,
+            text = "Never"
+        )
+        Text(
+            modifier = Modifier.weight(1f),
+            textAlign = TextAlign.Center,
+            fontSize = 14.sp,
+            text = "Sometimes"
+        )
+        Text(
+            modifier = Modifier.weight(1f),
+            textAlign = TextAlign.End,
+            fontSize = 14.sp,
+            text = "Very often"
+        )
     }
 }
